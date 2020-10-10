14f608868567466660d7db8df5ffea3bc4201585
Carlo Cabrales <carlocabrales@Carlos-MBP.home>
2020-10-10 11:13:40 -0400

    adding layouts

---
+++ src/main/java/com/rivaterrace/shopchop/configuration/LayoutConfiguration.java
@@ -0,0 +1,14 @@
+package com.rivaterrace.shopchop.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@Configuration
+public class LayoutConfiguration {
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
+	}
+}
