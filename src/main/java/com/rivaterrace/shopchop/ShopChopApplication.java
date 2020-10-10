54b8803f1d9191e878630283fff179510dd43b80
Carlo Cabrales <carlocabrales@Carlos-MBP.home>
2020-10-10 09:54:49 -0400

    spring init w/ updated gitignore

---
+++ src/main/java/com/rivaterrace/shopchop/ShopChopApplication.java
@@ -0,0 +1,13 @@
+package com.rivaterrace.shopchop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopChopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopChopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rivaterrace/shopchop/ShopChopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rivaterrace.shopchop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopChopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
